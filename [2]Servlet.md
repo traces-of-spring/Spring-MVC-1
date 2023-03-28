a5e4606672312f1ec6bb74ca96aefb34fd5024c3
yeGenieee <cyj95428@gmail.com>
2023-03-28 23:54:13 +0900

    Add Ch2-Hello Servlet

--- [2]Servlet.md
+++ [2]Servlet.md
@@ -12,3 +12,19 @@
 2. 내장 톰캣 서버는 내부에 서블릿 컨테이너 기능을 가지고 있어서, 서블릿 컨테이너를 통해 서블릿을 생성한다
 3. HTTP 요청이 들어오면, WAS (Web Application Server)는 HTTP 요청이 들어오면 HTTP 요청 메시지를 기반으로 request 객체를 생성한다
 4. WAS는 Response 객체 정보로 HTTP 응답 정보를 생성하여 반환한다
+
+
+## HttpServletRequest
+- HTTP 요청 메시지를 개발자가 직접 파싱해서 사용해도 되지만, 불편하다 
+- 서블릿은 개발자가 HTTP Request 메시지를 편리하게 사용할 수 있도록 HTTP Request 메시지를 파싱한다
+- 그리고 그 결과를 `HttpServletRequest` 객체에 담아서 제공한다
+
+### 임시 저장소 기능
+- 저장 : `request.setAttribute(name, value)`
+- 조회 : `request.getAttribute(name)`
+
+### 세션 관리 기능
+- `request.getSession(create: true)`
+
+## HttpServletRequest 사용 방법
+
---
+++ servlet/src/main/java/com/hello/servlet/basic/request/RequestHeaderServlet.java
@@ -0,0 +1,31 @@
+package com.hello.servlet.basic.request;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(name = "requestHeaderServlet", urlPatterns = "/request-header")
+public class RequestHeaderServlet extends HttpServlet {
+
+    @Override
+    protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String method = request.getMethod();
+        printStartLine(request, response);
+    }
+
+    private static void printStartLine(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        System.out.println("RequestHeaderServlet.service");
+        System.out.println("request = " + request);
+        System.out.println("response = " + response);
+
+        String username = request.getParameter("username");
+        System.out.println("username = " + username);
+
+        response.setContentType("text/plain");
+        response.setCharacterEncoding("utf-8");
+        response.getWriter().write("hello " + username);
+    }
+}
